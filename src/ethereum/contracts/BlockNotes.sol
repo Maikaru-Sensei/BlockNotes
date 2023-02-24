d9a2bf2eaf284c7f0d86b3b218b6455978745281
Michael Zauner <mzauner98@gmail.com>
2023-02-24 16:25:23 +0100

    init android application

--- src/ethereum/contracts/BlockNotes.sol
+++ src/ethereum/contracts/BlockNotes.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.11.0;
 
+/**
+ * Store all notes of all addresses in this contract
+ * @title BlockNotes contract
+**/
 contract BlockNotes {
 
+    /**
+     * represents a single note
+     * @param noteId unique identifier for a note
+     * @param message message which should be stored
+     * @param date datetime of taking the note (epoche)
+     * @param location location of taking the note
+    **/
     struct Note {
         string noteId;
         string message;
@@ -26,7 +37,7 @@ contract BlockNotes {
         } 
     }
 
-    function isUpdate(address _owner, string memory _noteId) view private returns(bool) {
+    function isUpdate(address _owner, string memory _noteId) private view returns(bool) {
         string[] memory notes = blockNotes[_owner];
 
         for (uint check = 0; check < notes.length; check++) {
@@ -38,7 +49,7 @@ contract BlockNotes {
         return false;
     }
 
-    function getNotes(address _owner) view public returns(Note[] memory) {
+    function getNotes(address _owner) public view returns(Note[] memory) {
         string[] memory noteIds = blockNotes[_owner];
 
         Note[] memory notes = new Note[](noteIds.length);
